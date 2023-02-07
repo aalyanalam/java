f8ab04029fa86bea1611fa20c569694aa36a1ba8
aalya <aalya@10.12.196.12>
2023-02-07 01:20:40 -0500

    t3

---
+++ test 3/src/test/pkg3/Test3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test.pkg3;
+
+/**
+ *
+ * @author aalya
+ */
+public class Test3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("something something cody rhodes");
+    }
+    
+}
